f5b942365b58b7caf503ca28a049056b0f2b40fb
Kerrinen Hope <8914+Ravendwyr@users.noreply.github.com>
2021-05-31 20:15:07 +0100

    Initial commit.

---
+++ LibIsClassic-1.0.lua
@@ -0,0 +1,51 @@
+
+-- ---------------------------------------------------------------------
+-- Getting Started
+
+assert(LibStub, "LibIsClassic-1.0 requires LibStub")
+
+local lib = LibStub:NewLibrary("LibIsClassic-1.0", 1)
+
+if not lib then return end
+
+local WOW_PROJECT_ID = _G.WOW_PROJECT_ID
+local WOW_PROJECT_MAINLINE = _G.WOW_PROJECT_MAINLINE
+local WOW_PROJECT_CLASSIC = _G.WOW_PROJECT_CLASSIC
+local WOW_PROJECT_BURNING_CRUSADE_CLASSIC = _G.WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+
+-- ---------------------------------------------------------------------
+-- Public API
+
+function lib:IsRetail()
+	-- One...
+	return WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+end
+
+function lib:IsClassic()
+	-- Two...
+	return WOW_PROJECT_ID == WOW_PROJECT_CLASSIC
+end
+
+function lib:IsBurningCrusadeClassic()
+	-- Five!
+	return WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC
+end
+
+-- ---------------------------------------------------------------------
+-- Embed Handling
+
+lib.embeds = lib.embeds or {}
+
+local mixins = { "IsRetail", "IsClassic", "IsBurningCrusadeClassic" }
+
+function lib:Embed(target)
+	for _, v in next, mixins do
+		target[v] = lib[v]
+	end
+	lib.embeds[target] = true
+	return target
+end
+
+for addon in next, lib.embeds do
+	lib:Embed(addon)
+end
